91ee1b78e3c25a718d49eaf6ed89068d6b7af733
eargaezx <eduardo.argaez@sngular.com>
2024-01-22 13:53:37 -0600

    Fix readit script

--- scripts/readit.py
+++ scripts/readit.py
@@ -144,7 +144,7 @@ def getCPUStats():
 
     arrData = re.split(':|[+]|;',cpuRow)
 
-    for i in  range(1,len(arrData)):
+    for i in  range(0,len(arrData)):
         value = re.split(' ',arrData[i])
         uIdValuesList.append(value[0])
 
